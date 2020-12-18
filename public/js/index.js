ad2327cb81a323f1d530b8b953d0339cf08a3bcc
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-12-18 17:04:13 -0500

    Added IndexedDB functionality for offline

--- public/js/index.js
+++ public/js/index.js
@@ -136,6 +136,7 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
+    alert('estoy pasando por aqui')
     saveRecord(transaction);
 
     // clear form
